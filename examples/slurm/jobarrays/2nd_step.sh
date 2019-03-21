b483939b397f756befe4d6d00be742438f1ee2f3
Javier Carnero <javier.carnero@atos.net>
2019-03-21 12:47:52 +0100

    Refactor of examples. Divided for gui/no-gui

--- examples/slurm/jobarrays/2nd_step.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-module purge
-module load gcc openmpi
-
-mpirun echo "2nd_step.sh: hello from host $(hostname), job $SLURM_ARRAY_JOB_ID, array index $SLURM_ARRAY_TASK_ID"
--- examples/up-gui.sh
+++ examples/up-gui.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 cd $1
-cp ../inputs-def.yaml ./
+cp ../inputs/gui/inputs-def.yaml ./
 cfy -v blueprints upload -b $1 blueprint.yaml
 rm inputs-def.yaml
 echo ''
-cfy deployments create -b $1 -i ../../inputs/local-blueprint-inputs.yaml --skip-plugins-validation $1
+cfy deployments create -b $1 -i ../inputs/gui/local-blueprint-inputs.yaml --skip-plugins-validation $1
 
 echo ''
 cfy executions start -d $1 install
