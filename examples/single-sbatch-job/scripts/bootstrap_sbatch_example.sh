7218a39f92e2616efe6fb86a732f6acdf76234e4
Javier Carnero <javier.carnero@atos.net>
2019-04-01 16:18:34 +0200

    Openstack plugin updated. Small bug solved.

--- examples/single-sbatch-job/scripts/bootstrap_sbatch_example.sh
+++ examples/single-sbatch-job/scripts/bootstrap_sbatch_example.sh
@@ -2,16 +2,24 @@
 
 FILE="touch.script"
 
+# cat > $FILE <<- EOM
+# #!/bin/bash -l
+
+# #SBATCH -p $2
+# #SBATCH -N 1
+# #SBATCH -n 1
+# #SBATCH --ntasks-per-node=1
+# #SBATCH -t 00:01:00
+
+# # DYNAMIC VARIABLES
+
+# touch test_$1.test
+# EOM
+
 cat > $FILE <<- EOM
 #!/bin/bash -l
-
-#SBATCH -p $2
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --ntasks-per-node=1
-#SBATCH -t 00:01:00
-
-# DYNAMIC VARIABLES
+#PBS -N 1
 
 touch test_$1.test
+
 EOM
